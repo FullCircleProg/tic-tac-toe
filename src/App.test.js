de6b8f5d82a2f298db0762079e23683834024a4d
FullCircleProg <fullcircledev@protonmail.com>
2022-08-20 15:28:52 +0200

    Test sample

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,20 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
+import * as components from './App';
+
+Object.keys(components).forEach(componentName => {
+  const Component = components[componentName];
+  describe(`Component: ${componentName}`, () => {
+    test(`${componentName} renders same as before with default props`, () => {
+      const wrapper = renderer
+        .create(<Component />)
+        .toJSON();
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+}); 
+
+/*import renderer from 'react-test-renderer';
 import App from './App';
 
 it('Renders same as before', () => {
@@ -6,7 +22,7 @@ it('Renders same as before', () => {
     .create(<App />)
     .toJSON();
   expect(component).toMatchSnapshot();
-});
+});*/
 
 
 
